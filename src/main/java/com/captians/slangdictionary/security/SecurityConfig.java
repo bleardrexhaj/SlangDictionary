0f24e51b3351e902415e0abce0dd27a0de5b7b58
Bleard <rexhajbleard@gmail.com>
2020-08-11 19:53:39 -0500

    SecurityConfig moved to a specific package.

--- src/main/java/com/captians/slangdictionary/security/SecurityConfig.java
+++ src/main/java/com/captians/slangdictionary/security/SecurityConfig.java
@@ -1,28 +1,41 @@
-/*
 package com.captians.slangdictionary.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter
-{
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    }
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth)
-            throws Exception
-    {
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("USER");
+    DataSource dataSource;
+
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select user,password,enabled from authentication where user = ?")
+                .authoritiesByUsernameQuery("select username, authority from authorities where username = ?");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .antMatchers("/term").hasRole("USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
-*/
