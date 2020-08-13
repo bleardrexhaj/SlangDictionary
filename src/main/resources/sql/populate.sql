4a6539a6b32232169666816008bd924c870a9a5d
erdenesaikhan <60585259+erdenesaikhan@users.noreply.github.com>
2020-08-13 10:00:45 -0500

    add category which is tech

--- src/main/resources/sql/populate.sql
+++ src/main/resources/sql/populate.sql
@@ -11,6 +11,8 @@ INSERT INTO category (id, name, description) values (1, "College", "Contains sim
 INSERT INTO category (id, name, description) values (2,"Food", "Contains food names around nowadays")
 INSERT INTO category (id, name, description) values (3, "Sports", "Contains sport type, equiment and so on")
 INSERT INTO category (id, name, description) values (4, "Name", "Contains name of worldwide ")
+INSERT INTO category (id, name, description) values (5, "Tech", "Contains tech words ")
+
 
 INSERT INTO term (id, author, definition, example, thumbs_down, thumbs_up, word, written_on, user_id, category_id) values (15, "Bleard", "Greeting!", "person 1: hello\r\nPerson 2: hi\r\nPerson 1: goodbye\r\nPerson 2: Farewell", 32, 64, "hello", '2020-08-05 00:00:00',1, 1)
 INSERT INTO term (id, author, definition, example, thumbs_down, thumbs_up, word, written_on, user_id, category_id) values (16, "Bleard", "To be/look like a gangster", "person 1: He is thugish", 32, 64, "thugish", '2020-08-06 00:00:00',1, 1)
