b71dd64b785fdd9aa1a65aa1a86addb256b0d80b
Bleard <rexhajbleard@gmail.com>
2020-08-13 23:32:23 -0500

    Bug Fix - Autogenerated key

--- src/main/java/com/captians/slangdictionary/model/User.java
+++ src/main/java/com/captians/slangdictionary/model/User.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @EmptyOrSize(message = "{User.name}")
